993d4f95cf995700f1919258c6097b72fd9af7d5
SahasPunchihewa <sahasmcg2995@gmail.com>
2021-03-31 07:25:40 +0530

    created room object

--- src/classv/Room.java
+++ src/classv/Room.java
@@ -1,4 +1,19 @@
 package classv;
 
-public class Room {
+public class Room
+{
+    private String cusName;
+
+    public Room(String cusName)
+    {
+        this.cusName = cusName;
+    }
+    public String getCusName()
+    {
+        return cusName;
+    }
+    public void setCusName(String cusName)
+    {
+        this.cusName = cusName;
+    }
 }
